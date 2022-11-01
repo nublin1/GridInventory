55913f6de27473d7e2b251363523eb97314c6b1b
alex <reddragon1151@hotmail.com>
2022-11-01 23:23:56 +0300

    work1

--- GridInventory/Assets/GridInventory/Scripts/Inventory/GridInventorySystem.cs
+++ GridInventory/Assets/GridInventory/Scripts/Inventory/GridInventorySystem.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using NaughtyAttributes;
+using System;
 
 public class GridInventorySystem : MonoBehaviour
 {
@@ -15,13 +17,33 @@ public class GridInventorySystem : MonoBehaviour
     [SerializeField] private float scrollBar_Width = 5f;
     [SerializeField] private float header_Heigth = 12f;
 
+    // Prefabs
+    [Header("Inventory Prefabs")]
+    [SerializeField] private bool setupPrefabs;
+    [ShowIf("setupPrefabs")]
+    [SerializeField] private GameObject cellPrefab;
+
     // Internal variables
     Vector2 halfCellSize;
     private GridXY_v2 grid;
 
+    Transform cellsContainer;
 
+    private void Awake()
+    {
+        cellsContainer = transform.Find("Scroll/Viewport/Slots");
+    }
+
+    private void Start()
+    {
+        grid = new GridXY_v2();
+    }
+
+    [Button("Generate inventory")]
     private void InventoryGeneration()
     {
+        ClearExitedCells();
+
         halfCellSize = new Vector2(cellSize / 2, cellSize / 2);
 
         if (viewportHeight > gridHeight)
@@ -32,8 +54,7 @@ public class GridInventorySystem : MonoBehaviour
 
         transform.GetComponent<RectTransform>().sizeDelta = new Vector2(viewportSize.x + cells_Scroll_Spacing + scrollBar_Width, viewportSize.y);
 
-        // Contains array of cells
-        Transform cellsContainer = transform.Find("Scroll/Viewport/Slots");
+        // Contains array of cells        
         cellsContainer.localScale = new Vector3(1f, 1f, 1f);
         cellsContainer.GetComponent<GridLayoutGroup>().cellSize = new Vector2(cellSize, cellSize);
         cellsContainer.GetComponent<GridLayoutGroup>().spacing = Vector2.zero;
@@ -47,5 +68,55 @@ public class GridInventorySystem : MonoBehaviour
         Transform scrollBar = transform.Find("Scroll/Scrollbar Vertical");
         scrollBar.GetComponent<RectTransform>().sizeDelta = new Vector2(scrollBar_Width, 0);
 
+        GenerateGrid();
+
+    }
+
+    private void ClearExitedCells()
+    {
+        if(cellsContainer == null)
+            cellsContainer = transform.Find("Scroll/Viewport/Slots");
+
+        if (cellsContainer.childCount > 0)
+        {
+            for (int i = 0; i < cellsContainer.childCount; i++)
+            {
+                DestroyImmediate(cellsContainer.GetChild(i).gameObject);
+            }
+        }
+    }
+
+    private void CreateBackground()
+    {
+        if (transform.Find("Background") == null)
+        {
+            GameObject background = new GameObject("Background");
+            background.transform.parent = transform;            
+
+            RectTransform rectBackground = background.GetComponent<RectTransform>();
+            rectBackground.anchorMin = new Vector2(0, 0);
+            rectBackground.anchorMax = new Vector2(1, 1);
+            rectBackground.pivot = new Vector2(0.5f, .5f);
+        }
+
+        if (transform.Find("Background Outline") == null)
+        {
+            GameObject backgroundOutline = new GameObject("Background Outline");
+            backgroundOutline.transform.parent = transform;
+
+            RectTransform rectBackgroundOutline = backgroundOutline.GetComponent<RectTransform>();
+            rectBackgroundOutline.anchorMin = new Vector2(0, 0);
+            rectBackgroundOutline.anchorMax = new Vector2(1, 1);
+            rectBackgroundOutline.pivot = new Vector2(0.5f, .5f);
+        }
+    }
+
+    private void GenerateGrid()
+    {
+        if (grid == null)
+        {
+            grid = new GridXY_v2();
+        }
+        grid.GenerateCells(gridWidth, gridHeight, cellSize, transform.position, cellPrefab, cellsContainer);
     }
 }
